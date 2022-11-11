81bea2b03ad644a9638f3d73ad57386176174fbc
Keenah <kvancamp@mail.com>
2022-11-11 14:16:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from fastapi import fastapi
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+  return {"message": "Hello World!!!"}
+
+@app.get("/posts")
+def get_posts():
+  return {"data": "This is your posts"}
+  
+
+
