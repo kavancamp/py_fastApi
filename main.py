367503c93e4695d74612bce1785f168d828fdd75
Keenah <kvancamp@mail.com>
2022-11-12 15:33:26 -0600

    import random/ created my_post / saved

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randrange
+
+
 
 app = FastAPI()
+
 class Post(BaseModel):
   title: str 
   content: str
   published: bool = True
   rating: Optional[int] = None
 
+my_posts =[{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "Favorite Foods", "content": "I like pizza", "id": 2}]
 
 #@ = decorator
 
@@ -18,10 +23,11 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-  return {"data": "This is your posts"}
+  return {"data": my_posts}
   
 @app.post("/post")
 def create_posts(post: Post):
-  print(post)
-  print(post.dict())
-  return {"data": post}
+  post_dict = post.dict()
+  post_dict['id'] = randrange(0, 1000000)
+  my_posts.append()
+  return {"data": post_dict}
