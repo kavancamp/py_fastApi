8ea52e733ece0760e7ddc415e6b7f55d7e7e8fac
Keenah <kvancamp@mail.com>
2022-11-12 13:40:41 -0600

    create posts/imported BaseModel

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
-from fastapi import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+class Post(BaseModel):
+  title: str 
+  content: str
+  published: bool = True
+  rating: Optional[int] = None
+
+
 #@ = decorator
 
 @app.get("/")
@@ -12,7 +20,8 @@ def root():
 def get_posts():
   return {"data": "This is your posts"}
   
-@app.post("/createposts")
-def create_posts(payload: dict = Body):
-  print(payload)
-  return {"message": "successfully created posts"}
+@app.post("/post")
+def create_posts(post: Post):
+  print(post)
+  print(post.dict())
+  return {"data": post}
