3bfd3004fc15b07d1771268d32e273b10953765d
Keenah <kvancamp@mail.com>
2022-11-11 19:15:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+#@ = decorator
 
 @app.get("/")
 def root():
@@ -10,5 +12,7 @@ def root():
 def get_posts():
   return {"data": "This is your posts"}
   
-
-
+@app.post("/createposts")
+def create_posts(payload: dict = Body):
+  print(payload)
+  return {"message": "successfully created posts"}
