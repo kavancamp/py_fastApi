cb89a098f90d797640f92af28c81eac720a79370
Keenah <kvancamp@mail.com>
2022-11-12 16:55:54 -0600

    added post by id request

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Post(BaseModel):
 
 my_posts =[{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "Favorite Foods", "content": "I like pizza", "id": 2}]
 
+def find_post(id):
+  for p in my_posts:
+    if p["id"] == id: 
+      return p
 #@ = decorator
 
 @app.get("/")
@@ -29,5 +33,11 @@ def get_posts():
 def create_posts(post: Post):
   post_dict = post.dict()
   post_dict['id'] = randrange(0, 1000000)
-  my_posts.append()
+  my_posts.append(post_dict)
   return {"data": post_dict}
+
+@app.get("/posts/{id}") #path parameter
+def get_post(id: int):
+  post = find_post(id) 
+  print(post)
+  return {"post_detail": f"Here is post {id}"}
